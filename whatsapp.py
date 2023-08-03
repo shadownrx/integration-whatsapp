c9ea3434369a07f1db8472b4cbd09aee6cdb7c04
salvador <salvadorjuars4@gmail.com>
2023-08-03 18:29:37 -0300

    first commit

---
+++ whatsapp.py
@@ -0,0 +1,16 @@
+
+import os
+from twilio.rest import Client
+
+
+account_sid = os.environ['Appi_key']
+auth_token = os.environ['Api_token']
+client = Client(account_sid, auth_token)
+
+message = client.messages.create(
+                              body='Hello there!',
+                              from_='whatsapp:+14155238886',
+                              to='whatsapp:+543816375805'
+                          )
+
+print(message.sid)
